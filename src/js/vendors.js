afee86feec8f5151a3591168782bb9a397cab330
Александр Рспономарев <sanya2128@gmail.com>
2018-11-16 22:07:44 +0500

    init

---
+++ src/js/vendors.js
@@ -0,0 +1,23 @@
+
+
+// Other vendor
+// ../sass/vendor/slick/slick.min.js
+// ../sass/vendor/jscrollpane/jquery.mousewheel.js
+// ../sass/vendor/fancy/jquery.fancybox.pack.js
+// ../sass/vendor/jscrollpane/jquery.jscrollpane.min.js
+// ../sass/vendor/range-slider/bootstrap-slider.min.js
+// ../sass/vendor/stycky-sidebar/ResizeSensor.min.js
+// ../sass/vendor/stycky-sidebar/theia-sticky-sidebar.min.js
+// ../sass/vendor/jQueryFormStyler/jquery.formstyler.min.js
+// ../sass/vendor/modernizr-custom.js
+//= ../sass/vendor/bootstrap/js/modal.js
+// ../sass/vendor/xzoom/xzoom.min.js
+// ../../bower_components/promise-polyfill/promise.min.js
+//= ../../node_modules/slick-carousel/slick/slick.min.js
+// ../../node_modules/perfect-scrollbar/dist/perfect-scrollbar.min.js
+// ../../node_modules/readmore-js/readmore.min.js
+// ../../bower_components/jquery.maskedinput/dist/jquery.maskedinput.min.js
+// ../../bower_components/lightgallery/dist/js/lightgallery-all.min.js
+// ../../node_modules/selectric/public/jquery.selectric.min.js
+// ../../node_modules/jscrollpane/script/jquery.jscrollpane.js
+// ../../node_modules/jscrollpane/script/jquery.mousewheel.js
